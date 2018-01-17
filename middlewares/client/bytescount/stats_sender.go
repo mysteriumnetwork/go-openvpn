e5f719693ea7df22ab6f90b61c0ddf0ec37b173a
Antanas Masevicius <antanas@mysterium.network>
2018-01-17 17:01:39 +0200

    add AuthenticateClient() stub to mysterium client

---
+++ middlewares/client/bytescount/stats_sender.go
@@ -0,0 +1,24 @@
+package bytescount
+
+import (
+	"github.com/mysterium/node/identity"
+	"github.com/mysterium/node/server"
+	"github.com/mysterium/node/server/dto"
+	"github.com/mysterium/node/session"
+)
+
+type SessionStatsSender func(bytesSent, bytesReceived int) error
+
+func NewSessionStatsSender(mysteriumClient server.Client, sessionId session.SessionId, signer identity.Signer) SessionStatsSender {
+	sessionIdString := string(sessionId)
+	return func(bytesSent, bytesReceived int) error {
+		return mysteriumClient.SendSessionStats(
+			sessionIdString,
+			dto.SessionStats{
+				BytesSent:     bytesSent,
+				BytesReceived: bytesReceived,
+			},
+			signer,
+		)
+	}
+}
