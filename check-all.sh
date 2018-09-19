d78331f4e002e1e6c795623caac8df837cb443f4
Tadas V <tadovas@gmail.com>
2018-09-19 15:50:23 +0300

    Fix another path

--- check-all.sh
+++ check-all.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-xgo -x -v --image=mysteriumnetwork/xgo-1.9.2 --targets=linux/amd64,darwin/amd64,windows/amd64,ios-10.3/arm64,android-21/arm64 --out=build/test $PWD/cmd
+xgo -x -v --image=mysteriumnetwork/xgo-1.9.2 --targets=linux/amd64,darwin/amd64,windows/amd64,ios-10.3/arm64,android-21/arm64 --out=build/test $PWD/examples
