94df1144d09a9175d715aa0b485a4f6e2715bef1
Viktoras Kuznecovas <viktoras@kuznecovas.net>
2018-10-01 11:33:18 +0300

    added missing copyright headers

--- openvpn/cmd_exit_signal.go
+++ openvpn/cmd_exit_signal.go
@@ -1,7 +1,7 @@
 // +build !windows
 
 /*
- * Copyright (C) 2017 The "MysteriumNetwork/node" Authors.
+ * Copyright (C) 2018 The "MysteriumNetwork/go-openvpn" Authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- openvpn/tunnel/setup_tun.go
+++ openvpn/tunnel/setup_tun.go
@@ -1,7 +1,7 @@
 // +build !linux
 
 /*
- * Copyright (C) 2018 The "MysteriumNetwork/node" Authors.
+ * Copyright (C) 2018 The "MysteriumNetwork/go-openvpn" Authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
