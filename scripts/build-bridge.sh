c072519172f92fc05fcccf4b935b3cff0719c49a
Tadas V <tadovas@gmail.com>
2018-11-19 16:04:27 +0200

    Enable crosscompiled android arm7 version of openvpn

--- scripts/build-bridge.sh
+++ scripts/build-bridge.sh
@@ -24,6 +24,7 @@ echo "O3 is: $O3"
 /go-src-root/scripts/x-compile-windows.sh
 /go-src-root/scripts/x-compile-ios.sh
 /go-src-root/scripts/x-compile-android-arm64.sh
+/go-src-root/scripts/x-compile-android-armeabi-v7a.sh
 /go-src-root/scripts/x-compile-android-x86.sh
 /go-src-root/scripts/x-compile-android-amd64.sh
 
