ab8c629947767a47df569b470c625528e70b1f72
anjmao <andzej.maciusovic@gmail.com>
2019-11-12 17:49:53 +0200

    WIP

--- rm/bb.sh
+++ rm/bb.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
-TOOLCHAIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64
-
-ARCH=aarch64
-export CC=$TOOLCHAIN/bin/$ARCH-linux-android21-clang
-export CXX=$TOOLCHAIN/bin/$ARCH-linux-android21-clang++
 export CGO_CXXFLAGS="-v"
 export CGO_CCFLAGS="-v"
-# export AR=$TOOLCHAIN/bin/$ARCH-linux-android-ar
-# export AS=$TOOLCHAIN/bin/$ARCH-linux-android-as
-# export LD=$TOOLCHAIN/bin/$ARCH-linux-android-ld
-# export RANLIB=$TOOLCHAIN/bin/$ARCH-linux-android-ranlib
-# export STRIP=$TOOLCHAIN/bin/$ARCH-linux-android-strip
-GOOS=android GOARCH=arm64 CGO_ENABLED=1 gomobile bind -target=android/arm64 github.com/mysteriumnetwork/go-openvpn/openvpn3
-# GOOS=android GOARCH=arm64 CGO_ENABLED=1 gobind -outdir ./build/app -lang="go,java" github.com/mysteriumnetwork/go-openvpn/openvpn3
-
-# ARCH=i686
-# export CC=$TOOLCHAIN/bin/$ARCH-linux-android28-clang
-# export CXX=$TOOLCHAIN/bin/$ARCH-linux-android28-clang++
-# export CGO_CXXFLAGS="-v"
-# export CGO_CCFLAGS="-v"
-# GOOS=android GOARCH=386 CGO_ENABLED=1 gomobile bind -target=android/386 github.com/mysteriumnetwork/go-openvpn/openvpn3
-
-# ARCH=armv7a
-# export CC=$TOOLCHAIN/bin/$ARCH-linux-androideabi28-clang
-# stat $CC
-# export CXX=$TOOLCHAIN/bin/$ARCH-linux-androideabi28-clang++
-# export AR=$TOOLCHAIN/bin/$ARCH-linux-android-ar
-# export AS=$TOOLCHAIN/bin/$ARCH-linux-android-as
-# export LD=$TOOLCHAIN/bin/$ARCH-linux-android-ld
-# export RANLIB=$TOOLCHAIN/bin/$ARCH-linux-android-ranlib
-# export STRIP=$TOOLCHAIN/bin/$ARCH-linux-android-strip
-# GOOS=android GOARCH=arm CGO_ENABLED=1 gomobile bind -target=android/arm github.com/mysteriumnetwork/go-openvpn/openvpn3
+gomobile bind -target=android/arm64 github.com/mysteriumnetwork/go-openvpn/test
--- rm/install.sh
+++ rm/install.sh
@@ -1,36 +1,17 @@
+
 TOOLCHAIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64
+echo "Bootstrapping android/armv7a..."
+ARCH=armv7a
+CC=$TOOLCHAIN/bin/$ARCH-linux-androideabi16-clang CXX=$TOOLCHAIN/bin/$ARCH-linux-androideabi16-clang++ GOOS=android GOARCH=arm CGO_ENABLED=1 go install std
+
+# echo "Bootstrapping android/arm64..."
+# ARCH=aarch64
+# CC=$TOOLCHAIN/bin/$ARCH-linux-android21-clang CXX=$TOOLCHAIN/bin/$ARCH-linux-android21-clang++ GOOS=android GOARCH=arm64 CGO_ENABLED=1 go install std
+
+# echo "Bootstrapping android/i686..."
 # ARCH=i686
-# #export AR=$TOOLCHAIN/bin/$ARCH-linux-android-ar
-# #export AS=$TOOLCHAIN/bin/$ARCH-linux-android-as
-# export CC=$TOOLCHAIN/bin/$ARCH-linux-android21-clang
-# export CXX=$TOOLCHAIN/bin/$ARCH-linux-android21-clang++
-# # export LD=$TOOLCHAIN/bin/$ARCH-linux-android-ld
-# # export RANLIB=$TOOLCHAIN/bin/$ARCH-linux-android-ranlib
-# # export STRIP=$TOOLCHAIN/bin/$ARCH-linux-android-strip
-# GOOS=android GOARCH=386 CGO_ENABLED=1 go install std
+# CC=$TOOLCHAIN/bin/$ARCH-linux-android21-clang CXX=$TOOLCHAIN/bin/$ARCH-linux-android21-clang++ GOOS=android GOARCH=386 CGO_ENABLED=1 go install std
 
-
-# ARCH=armv7a
-# export AR=$TOOLCHAIN/bin/$ARCH-linux-android-ar
-# export AS=$TOOLCHAIN/bin/$ARCH-linux-android-as
-# export CC=$TOOLCHAIN/bin/$ARCH-linux-androideabi28-clang
-# export CXX=$TOOLCHAIN/bin/$ARCH-linux-androideabi28-clang++
-# export LD=$TOOLCHAIN/bin/$ARCH-linux-android-ld
-# export RANLIB=$TOOLCHAIN/bin/$ARCH-linux-android-ranlib
-# export STRIP=$TOOLCHAIN/bin/$ARCH-linux-android-strip
-# GOOS=android GOARCH=arm CGO_ENABLED=1 go install std
-
-ARCH=aarch64
-export CC=$TOOLCHAIN/bin/$ARCH-linux-android21-clang
-export CXX=$TOOLCHAIN/bin/$ARCH-linux-android21-clang++
-GOOS=android GOARCH=arm64 CGO_ENABLED=1 go install std
-
-# ARCH=i686
-# export AR=$TOOLCHAIN/bin/$ARCH-linux-android-ar
-# export AS=$TOOLCHAIN/bin/$ARCH-linux-android-as
-# export CC=$TOOLCHAIN/bin/$ARCH-linux-android28-clang
-# export CXX=$TOOLCHAIN/bin/$ARCH-linux-android28-clang++
-# export LD=$TOOLCHAIN/bin/$ARCH-linux-android-ld
-# export RANLIB=$TOOLCHAIN/bin/$ARCH-linux-android-ranlib
-# export STRIP=$TOOLCHAIN/bin/$ARCH-linux-android-strip
-# GOOS=android GOARCH=386 CGO_ENABLED=1 go install std
+# echo "Bootstrapping android/am64..."
+# ARCH=x86_64
+# CC=$TOOLCHAIN/bin/$ARCH-linux-android21-clang CXX=$TOOLCHAIN/bin/$ARCH-linux-android21-clang++ GOOS=android GOARCH=amd64 CGO_ENABLED=1 go install std
