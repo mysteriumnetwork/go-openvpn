29ec0b14ad974417a34c83056db0346845fdeb09
Dmitry Shihovtsev <soffokulus@gmail.com>
2020-02-04 13:01:12 +0600

    Use new XGO image and migrate to go modules

--- check-all.sh
+++ check-all.sh
@@ -17,7 +17,31 @@ function verifyAndroidLib {
     return 0
 }
 
-./xgo-check.sh --targets=linux/amd64,darwin/amd64,windows/amd64 --out=build/desktop $GOPATH/src/github.com/mysteriumnetwork/go-openvpn/examples/desktop
-./xgo-check.sh --targets=ios/*,android/* --out=build/mobile $GOPATH/src/github.com/mysteriumnetwork/go-openvpn/examples/mobile
+docker run --rm \
+    -v "$PWD"/build:/build \
+    -v "$GOPATH"/.xgo-cache:/deps-cache:ro \
+    -v "$PWD":/source \
+    -e OUT=desktop \
+    -e FLAG_V=false \
+    -e FLAG_X=false \
+    -e FLAG_RACE=false \
+    -e FLAG_BUILDMODE=default \
+    -e TARGETS=linux/amd64,darwin/amd64,windows/amd64 \
+    mysteriumnetwork/xgo:1.13.6 github.com/mysteriumnetwork/go-openvpn/examples/desktop
+
+
+docker run --rm \
+    -v "$PWD"/build:/build \
+    -v "$GOPATH"/.xgo-cache:/deps-cache:ro \
+    -v "$GOPATH"/src:/ext-go/1/src:ro \
+    -e OUT=mobile \
+    -e FLAG_V=false \
+    -e FLAG_X=false \
+    -e FLAG_RACE=false \
+    -e FLAG_BUILDMODE=default \
+    -e TARGETS=android/* \
+    -e EXT_GOPATH=/ext-go/1 \
+    -e GO111MODULE=off \
+    mysteriumnetwork/xgomobile:1.13.6 github.com/mysteriumnetwork/go-openvpn/examples/mobile
 
 verifyAndroidLib build/mobile.aar x86 x86_64 arm64-v8a armeabi-v7a
--- xgo-check.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-[[ -z `which xgo` ]] || go get -u github.com/karalabe/xgo
-
-PATH=$PATH:$GOPATH/bin xgo --image=mysteriumnetwork/xgo:1.11 $@
