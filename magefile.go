97d1ee61442aeb0f42da095d1225b70241cae784
Valdas Petrulis <2713911+Waldz@users.noreply.github.com>
2020-12-21 17:37:12 +0200

    License under AGPLV3
    Licensing under same license as https://github.com/OpenVPN/openvpn3
    
    Signed-off-by: Valdas Petrulis <petrulis.valdas@gmail.com>

--- magefile.go
+++ magefile.go
@@ -1,5 +1,24 @@
 // +build mage
 
+/*
+ * go-openvpn -- Go gettable library for wrapping Openvpn functionality in go way.
+ *
+ * Copyright (C) 2020 The "MysteriumNetwork/go-openvpn" Authors..
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License Version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program in the COPYING file.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package main
 
 import (
--- openvpn3/tun_setup_bridge.go
+++ openvpn3/tun_setup_bridge.go
@@ -1,18 +1,20 @@
 /*
- * Copyright (C) 2018 The "MysteriumNetwork/go-openvpn" Authors.
+ * go-openvpn -- Go gettable library for wrapping Openvpn functionality in go way.
+ *
+ * Copyright (C) 2020 The "MysteriumNetwork/go-openvpn" Authors..
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Affero General Public License Version 3
+ * as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program in the COPYING file.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 
 package openvpn3
